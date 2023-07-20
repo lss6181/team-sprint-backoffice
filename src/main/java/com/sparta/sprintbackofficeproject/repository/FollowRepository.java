66d7cef359f174252c321e6931e1847190bd8713
lss6181 <dexter5ya@naver.com>
2023-07-20 16:32:41 +0900

    팔로우 기능 수정 - 팔로우/언팔로우 성공메세지 나오도록 수정 - 프로필 조회에서 팔로워(총 팔로워 수, 팔로워들 username List)와 팔로잉(총 팔로잉 수, 팔로잉 한 username List) 함께 나오도록 구현

--- src/main/java/com/sparta/sprintbackofficeproject/repository/FollowRepository.java
+++ src/main/java/com/sparta/sprintbackofficeproject/repository/FollowRepository.java
@@ -4,8 +4,13 @@ import com.sparta.sprintbackofficeproject.entity.Follow;
 import com.sparta.sprintbackofficeproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 	Optional<Follow> findByFollowerUserAndFollowingUser(User followUser, User followingUser);
+
+	List<Follow> findAllByFollowerUser(User user);
+
+	List<Follow> findAllByFollowingUser(User user);
 }

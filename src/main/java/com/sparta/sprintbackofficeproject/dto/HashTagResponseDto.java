d5fe79dddce7b07163f7e984b56e129c532e8c07
Jung Haein <haeinj1215@gmail.com>
2023-07-20 19:25:17 +0900

    해시태그 에러 -> HashTagResponseDto에 @Getter 추가

--- src/main/java/com/sparta/sprintbackofficeproject/dto/HashTagResponseDto.java
+++ src/main/java/com/sparta/sprintbackofficeproject/dto/HashTagResponseDto.java
@@ -1,7 +1,9 @@
 package com.sparta.sprintbackofficeproject.dto;
 
 import com.sparta.sprintbackofficeproject.entity.HashTag;
+import lombok.Getter;
 
+@Getter
 public class HashTagResponseDto {
 	private Long hashTagId;
 	private String hashTag;

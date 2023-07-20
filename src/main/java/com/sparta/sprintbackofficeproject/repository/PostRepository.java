cb7de2947d0c66a2389ef1744b743b2d7758f643
Jung Haein <haeinj1215@gmail.com>
2023-07-20 14:46:28 +0900

    타임라인 조회- 게시글 중복 제거

--- src/main/java/com/sparta/sprintbackofficeproject/repository/PostRepository.java
+++ src/main/java/com/sparta/sprintbackofficeproject/repository/PostRepository.java
@@ -18,19 +18,20 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "            left join follows f2 on lp.user_id = f2.following_user_id\n" +
             "            left join tag_user_in_post tuip on p.id = tuip.post_id\n" +
             "            where (lp.created_at in (select min(lp.created_at)\n" +
-            "                                    from post p left join likes_post lp\n" +
-            "                                    on p.id = lp.post_id\n" +
-            "                                    where (p.user_id = ?1)))\n" +
+            "               from post p left join likes_post lp\n" +
+            "               on p.id = lp.post_id\n" +
+            "               where (p.user_id = ?1)))\n" +
             "            or ((p.user_id = ?1) and (lp.created_at is null))\n" +
             "            or (lp.user_id = ?1)\n" +
             "            or (lp.created_at in (select min(lp.created_at)\n" +
-            "                                  from  post p\n" +
-            "                                    left join follows f on p.user_id = f.following_user_id\n" +
-            "                                    left join likes_post lp on p.id = lp.post_id\n" +
-            "                                  where (f.follower_user_id = ?1)))\n" +
+            "                from  post p\n" +
+            "                left join follows f on p.user_id = f.following_user_id\n" +
+            "                left join likes_post lp on p.id = lp.post_id\n" +
+            "                where (f.follower_user_id = ?1)))\n" +
             "            or((f.follower_user_id = ?1)and (lp.created_at is null))\n" +
             "            or((f2.follower_user_id = ?1) and (lp.user_id = f2.following_user_id))\n" +
             "            or(tuip.user_id = ?1)\n" +
-            "            order by (if(p.created_at < lp.created_at, lp.created_at, p.created_at)) desc", nativeQuery = true)
+            "            group by p.id, p.created_at\n" +
+            "            order by (if(max(lp.created_at) > p.created_at, max(lp.created_at), p.created_at)) desc", nativeQuery = true)
     List<Post> findPost1(Long id);
 }
